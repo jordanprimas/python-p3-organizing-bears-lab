7b586da38835e0ccf094cc1510fa69f6631907df
Jordan Primas <jorprimas20@gmail.com>
2024-01-23 11:46:43 -0800

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Mr. Chocolate', 5, 'M', 'brown', 't2', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Rowdy', 5, 'M', 'brown', 't1', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Tabitha', 6, 'F', 'black', 't1', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Sergeant Brown', 10, 'M', 'black', 't2', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Melissa', 7, 'F', 'white', 't3', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Grinch', 4, 'M', 'green', 't2', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Wendy', 9, 'F', 'black', 't3', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 5, 'M', 'brown', 't4', True);
